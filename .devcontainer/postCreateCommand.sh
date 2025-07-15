7e147d5eab4ac6b82814eed962dd413e376e8e85
Christoph Krause <jan_christoph.krause@dfki.de>
2025-07-15 11:20:50 +0200

    rosdistro noetic

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,9 +4,9 @@
 cp -r /seerep/src/.devcontainer/.vscode/ /seerep/.vscode/
 
 #build the workspace with catkin
-catkin config  --workspace /seerep --cmake-args -Wno-deprecated -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-Wall -Wextra -fPIC -DBOOST_LOG_DYN_LINK"
+catkin config --workspace /seerep --cmake-args -Wno-deprecated -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-Wall -Wextra -fPIC -DBOOST_LOG_DYN_LINK"
 sudo apt update
-rosdep update
+rosdep update --rosdistro=noetic
 rosdep install --from-paths /seerep/src --ignore-src -y
 catkin build --workspace /seerep
 
